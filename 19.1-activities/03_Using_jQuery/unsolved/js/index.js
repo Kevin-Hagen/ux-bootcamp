71d9ae7d586f82745f5bebf055e209ad23be68d9
Kevin-Hagen <kevinh9878@gmail.com>
2023-04-17 21:17:49 -0500

    javascript and jquery practice

---
+++ 19.1-activities/03_Using_jQuery/unsolved/js/index.js
@@ -0,0 +1,11 @@
+// Use jQuery to target the <h1> tag and make it red.
+$("h1").css("color", "red");
+$("p").css("color", "orange");
+
+// Use jQuery to target the blue class and make it blue.
+$(".blue").css("color", "blue");
+
+// Use jQuery to target the addMe ID and change its HTML to Hello.
+$("#addMe").html('This is not a taco cat');
+
+$("p").css("background-color red");
